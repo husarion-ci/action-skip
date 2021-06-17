c20518ae6e7fe4a962eef13dbaccebd3de8a70fd
kamilmachon <kamil.machon@husarion.com>
2021-06-17 15:14:24 +0200

    add skip-argument action

---
+++ get_skip_argument.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+print(sys.argv)
+if len(sys.argv) == 2:
+    arg = sys.argv[1].split("skip::")
+    if len(arg) > 1:
+        arg = arg[1].split()[0]
+    else:
+        print("No skip instance found.")
+        sys.exit(0)
+        
+    with open("skip_arg.txt", "w") as arg_file:
+        arg_file.write(arg)
+
+else:
+    print("Incorrect number of argument!")
+    sys.exit(1)
